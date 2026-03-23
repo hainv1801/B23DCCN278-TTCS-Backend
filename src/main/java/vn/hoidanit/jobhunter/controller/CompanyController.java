e81acbf7c458c25893f45ac80901c9aa672c2bc3
hainv1801 <nguyenviet14921@gmail.com>
2026-03-23 15:41:31 +0700

    video 68

---
+++ src/main/java/vn/hoidanit/jobhunter/controller/CompanyController.java
@@ -0,0 +1,29 @@
+package vn.hoidanit.jobhunter.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import vn.hoidanit.jobhunter.domain.Company;
+import vn.hoidanit.jobhunter.service.CompanyService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+public class CompanyController {
+    private final CompanyService companyService;
+
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @PostMapping("/companies")
+    public ResponseEntity<Company> createCompany(@Valid @RequestBody Company company) {
+        // TODO: process POST request
+        Company newCompany = this.companyService.handleCreateCompany(company);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newCompany);
+    }
+
+}
